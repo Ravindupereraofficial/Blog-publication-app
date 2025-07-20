e635c3ed763e26785ef2387b19acbfeef43169c5
Ravindu Perera <ravindurandika2004@gmail.com>
2025-07-20 23:20:15 +0530

    debug the deployment error

--- app/api/stripe-checkout/route.ts
+++ app/api/stripe-checkout/route.ts
@@ -2,7 +2,7 @@ export const dynamic = "force-dynamic";
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2022-11-15' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2025-06-30.basil' });
 
 export async function POST(req: NextRequest) {
   try {
